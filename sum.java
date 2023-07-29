514229b3cf9bd0bc239d14ea6dff512c68ef4ee3
abhishek pathak <imabhishek6027@gmail.com>
2023-07-29 19:27:04 +0530

    initial commit

---
+++ sum.java
@@ -0,0 +1,8 @@
+public class sum {
+    public static void main  (String[] args) {
+        //varibales
+        int a=500,b=350,sum;
+        sum=a+b;
+        System.out.print("the sum of num is:"+sum );
+    }
+}
