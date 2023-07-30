dd14a76a2eadadd6f8ab90865198fea79545ce5d
abhishek pathak <imabhishek6027@gmail.com>
2023-07-30 22:53:26 +0530

    added multiply.java

---
+++ multiply.java
@@ -0,0 +1,8 @@
+public class multiply {
+    public static void main  (String[] args) {
+        //multiply  two numbers
+        int a=5;
+        int b=3;
+        System.out.print("the product is"+(a*b));
+    }
+}
