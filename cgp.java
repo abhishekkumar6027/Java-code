bfe004bd3c19d3ce436d8dc1389a782519127a21
abhishek pathak <imabhishek6027@gmail.com>
2023-08-03 18:37:50 +0530

    added cgp.java

---
+++ cgp.java
@@ -0,0 +1,12 @@
+public class cgp {
+    public static void main(String[] args) {
+       float subject1 = 32;
+       float subject2 = 46;
+       float subject3 = 50;
+
+       float cgp = (subject1 + subject2 + subject3)/30;
+       System.out.println("/cgp"); 
+
+
+    }
+}
